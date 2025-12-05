c269fe87315298d2538e25308170ac402ef52a5b
Leonardo Arias Paz <leoariaspaz@gmail.com>
2025-12-05 13:37:39 -0300

    Se cambia el estilo de algunas propiedades desde la forma CSS a la forma react

--- src/utils/Icons.jsx
+++ src/utils/Icons.jsx
@@ -4,12 +4,12 @@ export const EDIT = (
     width="16"
     height="16"
     fill="currentColor"
-    class="bi bi-pencil-square"
+    className="bi bi-pencil-square"
     viewBox="0 0 16 16"
   >
     <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
     <path
-      fill-rule="evenodd"
+      fillRule="evenodd"
       d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"
     />
   </svg>
@@ -21,7 +21,7 @@ export const DELETE = (
     width="16"
     height="16"
     fill="currentColor"
-    class="bi bi-trash"
+    className="bi bi-trash"
     viewBox="0 0 16 16"
   >
     <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
@@ -35,7 +35,7 @@ export const PRINT = (
     width="16"
     height="16"
     fill="currentColor"
-    class="bi bi-printer"
+    className="bi bi-printer"
     viewBox="0 0 16 16"
   >
     <path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1" />
@@ -49,7 +49,7 @@ export const SEARCH = (
     width="16"
     height="16"
     fill="currentColor"
-    class="bi bi-search"
+    className="bi bi-search"
     viewBox="0 0 16 16"
   >
     <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
@@ -59,7 +59,7 @@ export const SEARCH = (
 export const DEAL = (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    enable-background="new 0 0 24 24"
+    enableBackground="new 0 0 24 24"
     height="24px"
     viewBox="0 0 24 24"
     width="24px"
