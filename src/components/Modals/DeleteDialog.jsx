560a64085d0016c3b124cf1225e91497415d8571
Leonardo Arias Paz <leoariaspaz@gmail.com>
2026-01-09 21:50:49 -0300

    Rediseño

--- src/components/Modals/DeleteDialog.jsx
+++ src/components/Modals/DeleteDialog.jsx
@@ -5,9 +5,13 @@
          >
       <div className="modal-dialog center-vertical">
         <div className="modal-content">
-          <div className="modal-body">
-            <h6 className="modal-title mb-1">Confirmar eliminación</h6>
-            <p>{deleteMessage}</p>
+          <div className="modal-header h5 py-2">
+            Confirmar eliminación
+          </div>
+          <div className="modal-body py-0">
+            {deleteMessage}
+          </div>
+          <div className="modal-footer py-2">
             <div className="buttons">
               <button type="button" className="btn btn-sm btn-danger" onClick={handleDelete}>
                 Eliminar
