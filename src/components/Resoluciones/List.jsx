2a9d702d745048696c65bc675334f189d536c053
Leonardo Arias Paz <leoariaspaz@gmail.com>
2026-01-22 19:11:50 -0300

    Se reemplaza el listado de resoluciones por la nueva versión de hacerlo.

--- src/components/Resoluciones/List.jsx
+++ src/components/Resoluciones/List.jsx
@@ -1,34 +1,48 @@
-import ErrorMessage from "../Shared/ErrorMessage";
-import Spinner from "../Shared/Spinner";
-import Container from "../Forms/Container";
-import useFormGrid from "../../hooks/useFormGrid";
 import { Resoluciones } from "../../utils/endpoints";
-import { RECORDS_PER_PAGE } from "../../utils/constants";
-import Grid from "./Grid";
+import { BUTTON_COLUMN, DATA_COLUMN, EDIT_BUTTON, DELETE_BUTTON, RECORDS_PER_PAGE } from "../../utils/constants";
+import { useTitle } from "../Shared/hooks/useTitle";
+import { fetchEndpointFactory, deleteEndpointFactory } from "../../utils/endpointFactory";
+import CustomList from "../Shared/CustomList";
+import DeleteMessage from "../Shared/DeleteMessage.jsx"
 
 const ListResoluciones = () => {
-	const { loading, data, error, currentPage, totalPages, setData, 
-		setCurrentPage } = useFormGrid(Resoluciones.findAll, RECORDS_PER_PAGE)
+	const getDeleteMessage = (r) =>
+		<DeleteMessage message={`¿Está seguro que desea eliminar esta resolución?`}
+			fields={r.descripcion} />
 
-	if (loading) {
-		return <Spinner/>
+	const rowGenerator = ({ data }) => {
+		return {
+			key: data.id,
+			columns: [
+				{ type: DATA_COLUMN, data: data.descripcion },
+				{ type: DATA_COLUMN, data: data.detalle.substring(0, 75) + (data.detalle.length > 75 ? "..." : "") },
+				{ type: BUTTON_COLUMN, buttons: [
+					{ type: EDIT_BUTTON, path: `/resoluciones/edit/${data.id}` },
+					{ type: DELETE_BUTTON, path: `/resoluciones/delete/${data.id}`, id: data.id, message: getDeleteMessage(data) },
+				] },
+			],
+		};
 	}
 
-	return (
-		<Container title={"Resoluciones"} newPath="/resoluciones/new">
-			{error ? (
-				<ErrorMessage message={error} />
-			) : (
-				<Grid
-					data={data}
-					currentPage={currentPage}
-					totalPages={totalPages}
-					setData={setData}
-					setCurrentPage={setCurrentPage}
-				/>
-			)}
-		</Container>
-	)
+	const onFetchData = fetchEndpointFactory(Resoluciones.findAll);
+
+	const onDeleteRow = deleteEndpointFactory(Resoluciones.delete);
+
+	const properties = {
+		title: "Resoluciones",
+		rowGenerator,
+		recordsPerPage: RECORDS_PER_PAGE,
+		showSearchBar: false,
+		pathToNew: "/resoluciones/new",
+	}
+
+	const events = {
+		onFetchData,
+		onDeleteRow,
+	}
+
+	useTitle("Resoluciones");
+	return CustomList(properties, events);
 };
 
 export default ListResoluciones;
