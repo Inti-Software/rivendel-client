53f4e6ad4cd63659e9a48fbd054930a93feace14
Leonardo Arias Paz <leoariaspaz@gmail.com>
2026-04-27 22:08:48 -0300

    Obtiene la URL base del backend desde un archivo .env.

--- src/api/http.js
+++ src/api/http.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
+import { BASE_URL } from './constants';
 
 //para usar con interceptores
 export const authHttp = axios.create({
-  baseURL: 'http://localhost:3000', 
+  baseURL: BASE_URL,
   withCredentials: true,            //save cookies in storage browser
 });
 
 export const publicHttp = axios.create({
-  baseURL: 'http://localhost:3000', 
+  baseURL: BASE_URL, 
   withCredentials: true,            //save cookies in storage browser 
 });
