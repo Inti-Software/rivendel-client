35fb091ed8551be0bb8f1ef6bad0d35bb04e0453
Leonardo Arias Paz <leoariaspaz@gmail.com>
2026-09-09 19:40:23 -0300

    Se mueve formatCuil al utils.js de Shared

--- src/components/Shared/utis.js
+++ src/components/Shared/utis.js
@@ -1,1 +1,7 @@
 export const isNew = (value) => (Number.isInteger(value) && value === 0);
+
+export function formatCuil(value = '') {
+  const digits = value.replace(/\D/g, '').padEnd(11, ' ');
+  return `${digits.slice(0, 2)}-${digits.slice(2, 10)}-${digits.slice(10, 11)}`;
+}
+
